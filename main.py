497eb1144b978976d4a568af6fb983a3581e5dfa
Ankur Divekar <ankur.divekar@gmail.com>
2023-02-05 16:44:22 +0530

    Added QR superimposed invite from template

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import streamlit as st
 import cv2
+from PIL import Image
 import numpy as np
+import qrcode
+from io import BytesIO
 
 tabs = st.tabs(["Door Entry Manager", "Invite Generator"])
-
-tab_invite = tabs[1]
-with tab_invite:
-    st.text("Enter the name of the person you want to invite")
-    name = st.text_input("Name")
-    st.button("Generate Invite")
-    if name:
-        st.text(f"Generating invite for {name}")
 
 tab_door = tabs[0]
 with tab_door:
@@ -27,3 +22,62 @@ if image is not None:
     st.write("Here!")
     st.write(data)
     st.image(cv2_img)
+
+tab_invite = tabs[1]
+with tab_invite:
+
+    st.text("Enter the name of the person you want to invite")
+    name = st.text_input("Name")
+    st.button("Generate Invite")
+    if name:
+        st.text(f"Generating invite for {name}")
+
+        # Generate invite here
+
+        logo = Image.open("Assets/logo.jpg")
+        # taking base width
+        basewidth = 100
+
+        # adjust image size
+        wpercent = basewidth / float(logo.size[0])
+        hsize = int((float(logo.size[1]) * float(wpercent)))
+        logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+        QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+
+        BookingName = "Ankur Divekar"
+        BookingMobile = "9923317669"
+        SeatsTotal = "12"
+
+        QRcode.add_data(f"{BookingName}, {BookingMobile}, {SeatsTotal}")
+        QRcode.make(fit=True)
+
+        QRimg = QRcode.make_image(
+            fill_color=(52, 52, 52),
+            back_color=(222, 222, 222),
+        ).convert("RGB")
+
+        # set size of QR code
+        pos = ((QRimg.size[0] - logo.size[0]) // 2, (QRimg.size[1] - logo.size[1]) // 2)
+        QRimg.paste(logo, pos)
+
+        QRimg2 = QRimg.resize((300, 300), Image.LANCZOS)
+
+        # Get invite image
+        invite = Image.open("Assets/InviteTemplate.bmp")
+
+        invite.paste(QRimg2, (730, 40))
+
+        # invite.save("Assets/YourInvite.png")
+
+        print("Invite generated!")
+
+        buf = BytesIO()
+        invite.save(buf, format="JPEG")
+        byte_im = buf.getvalue()
+
+        btn = st.download_button(
+            label="Download Image",
+            data=byte_im,
+            file_name="YourInvitation.jpg",
+            mime="image/jpeg",
+        )
