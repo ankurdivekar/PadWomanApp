7713af13800b776036b17b8deb9be29c64519379
Ankur Divekar <ankur.divekar@gmail.com>
2023-02-10 21:09:21 +0530

    Added single record retrieval

--- db_operations.py
+++ db_operations.py
@@ -21,7 +21,6 @@ def create_connection(db_file):
 
 
 def show_db():
-
     with create_connection(st.secrets["db_file"]) as conn:
         st.write(conn)  # success message?
 
@@ -53,6 +52,32 @@ def get_seats_available(current_uuid):
         # print(f"Seats for {current_uuid}: {query}")
         seats_available = 0 if query is None else query[0] - query[1]
         return seats_available
+
+
+def get_booking_status(booking_phone):
+    with create_connection(st.secrets["db_file"]) as conn:
+
+        query = conn.execute(
+            f"SELECT * FROM {st.secrets['table_name']} WHERE BookingPhone ='{booking_phone}'"
+        )
+        print(query.fetchall())
+        if query.fetchall():
+            cols = [column[0] for column in query.description]
+            results_df = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+            st.dataframe(results_df)
+        else:
+            st.write(f"No booking found for {booking_phone}!")
+
+        # query = conn.execute(
+        #     f"SELECT * FROM {st.secrets['table_name']} WHERE BookingPhone = '{booking_phone}'"
+        # ).fetchone()
+        # if query is None:
+        #     st.write("No booking found")
+        # else:
+        #     st.write(query)
+        #     # st.write(f"Seats available: {query[0] - query[1]}")
+        #     # st.write(f"Seats occupied: {query[1]}")
+        #     # st.write(f"Seats total: {query[0]}")
 
 
 def update_seats_occupied(current_uuid, seats_occupied):
@@ -90,25 +115,47 @@ def reinitialize_db():
 
 
 def insert_row_into_db(bname, bmobile, seats_total):
-    uuid_tmp = str(uuid.uuid1())
-    with create_connection(st.secrets["db_file"]) as conn:
-        cur = conn.cursor()
 
-        cur.execute(
-            "INSERT OR IGNORE INTO Registration (UUID, BookingName, BookingPhone, SeatsTotal, SeatsOccupied) VALUES (?, ?, ?, ?, ?)",
-            (uuid_tmp, bname, bmobile, 0, 0),
-        )
-        cur.execute(
-            "UPDATE Registration SET SeatsTotal = SeatsTotal + ? WHERE BookingPhone = ?",
-            (seats_total, bmobile),
-        )
+    with create_connection(st.secrets["db_file"]) as conn:
+
+        # Search for existing entry
+        query = conn.execute(
+            f"SELECT UUID FROM {st.secrets['table_name']} WHERE BookingPhone = '{bmobile}'"
+        ).fetchone()
+
+        cur = conn.cursor()
+        if query is None:
+            # Insert new entry if not found
+            uuid_tmp = str(uuid.uuid1())
+            cur.execute(
+                "INSERT INTO Registration (UUID, BookingName, BookingPhone, SeatsTotal, SeatsOccupied) VALUES (?, ?, ?, ?, ?)",
+                (uuid_tmp, bname, bmobile, seats_total, 0),
+            )
+        else:
+            # Update existing entry if found
+            uuid_tmp = query[0]
+            cur.execute(
+                "UPDATE Registration SET SeatsTotal = SeatsTotal + ? WHERE BookingPhone = ?",
+                (seats_total, bmobile),
+            )
+
+        conn.commit()
+        # cur = conn.cursor()
+        # cur.execute(
+        #     "INSERT OR IGNORE INTO Registration (UUID, BookingName, BookingPhone, SeatsTotal, SeatsOccupied) VALUES (?, ?, ?, ?, ?)",
+        #     (uuid_tmp, bname, bmobile, 0, 0),
+        # )
+        # cur.execute(
+        #     "UPDATE Registration SET SeatsTotal = SeatsTotal + ? WHERE BookingPhone = ?",
+        #     (seats_total, bmobile),
+        # )
 
         # cur.execute(
         #     "INSERT INTO Registration (UUID, BookingName, BookingPhone, SeatsTotal, SeatsOccupied) VALUES (?, ?, ?, ?, ?)",
         #     (uuid_tmp, bname, bmobile, seats_total, "0"),
         # )
-        conn.commit()
-    st.session_state.uuid_tmp = uuid_tmp
+
+    return uuid_tmp
 
 
 def upload_data():
